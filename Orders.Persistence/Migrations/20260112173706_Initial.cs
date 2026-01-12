fbd6561d3c834c1fc641aece9aca8acc385f9472
Volodymyr <alternativeqoob@gmail.com>
2026-01-12 19:39:14 +0200

    Added initial migration

---
+++ Orders.Persistence/Migrations/20260112173706_Initial.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Orders.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "customer_orders",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CustomerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    StoreId = table.Column<int>(type: "integer", nullable: false),
+                    total_amount = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    total_currency = table.Column<string>(type: "character varying(3)", maxLength: 3, nullable: false),
+                    Status = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    ConfirmedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    PaidAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    CancelledAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    currency_code = table.Column<string>(type: "character varying(3)", maxLength: 3, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_customer_orders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "outbox_messages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    occurred_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    type = table.Column<string>(type: "character varying(512)", maxLength: 512, nullable: false),
+                    payload_json = table.Column<string>(type: "text", nullable: false),
+                    processed_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    attempts = table.Column<int>(type: "integer", nullable: false),
+                    last_error = table.Column<string>(type: "character varying(2000)", maxLength: 2000, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_outbox_messages", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "order_items",
+                columns: table => new
+                {
+                    product_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    order_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name_snapshot = table.Column<string>(type: "text", nullable: false),
+                    unit_price_amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    unit_price_currency_code = table.Column<string>(type: "text", nullable: false),
+                    quantity = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_order_items", x => new { x.order_id, x.product_id });
+                    table.ForeignKey(
+                        name: "FK_order_items_customer_orders_order_id",
+                        column: x => x.order_id,
+                        principalTable: "customer_orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_customer_orders_CustomerId",
+                table: "customer_orders",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_customer_orders_StoreId",
+                table: "customer_orders",
+                column: "StoreId");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_outbox_processed_at",
+                table: "outbox_messages",
+                column: "processed_at");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "order_items");
+
+            migrationBuilder.DropTable(
+                name: "outbox_messages");
+
+            migrationBuilder.DropTable(
+                name: "customer_orders");
+        }
+    }
+}
